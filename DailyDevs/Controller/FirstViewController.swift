55b56122c78dc67e3791fba1fd29a4bedf9e4378
tifoaudy <165150200111168@mail.ub.ac.id>
2019-02-26 19:02:29 +0700

    initial project

---
+++ DailyDevs/Controller/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DailyDevs
+//
+//  Created by Tifo Audi Alif Putra on 26/02/19.
+//  Copyright © 2019 BCC FILKOM. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DailyDevs/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DailyDevs
+//
+//  Created by Tifo Audi Alif Putra on 26/02/19.
+//  Copyright © 2019 BCC FILKOM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
